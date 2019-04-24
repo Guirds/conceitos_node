b546439df30cc36b83b44d7ccc5047c82c74d4c2
Guilherm Santos <gui07015@gmail.com>
2019-04-24 09:30:11 -0300

    Configurando ambiente

--- nunjucks.js
+++ nunjucks.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
-//Fica monitrando os arquivos
-nunjucks.configure("views", {
+// Fica monitrando os arquivos
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "njk"); //Seta uma variavel global, nessa caso sera como vamos chamar o nunjucks
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk') // Seta uma variavel global, nessa caso sera como vamos chamar o nunjucks
 
-const users = ["Guilherme Santos", "Diego Fernandes", "Cleiton Souza"];
+const users = ['Guilherme Santos', 'Diego Fernandes', 'Cleiton Souza']
 
-app.get("/", (req, res) => {
-  return res.render("list", { users });
-});
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
 
-app.get("/new", (req, res) => {
-  return res.render("new");
-});
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
 
-app.post("/create", (req, res) => {
-  users.push(req.body.user);
-  return res.redirect("/");
-});
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
 
-app.listen(3333);
+app.listen(3333)
